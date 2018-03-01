ced4661c7671c30fd3d5d2ef78c34fefcb29460d
Alberto Aragon <alberto@alturasoluciones.com>
2018-03-01 17:20:15 -0500

    Adding awsConfig variable for AWS Cognito authentication settings and testing signup and signin

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify';
+import awsConfig from './src/aws-exports';
 
-export default class App extends React.Component {
+Amplify.configure(awsConfig);
+
+import { withAuthenticator } from 'aws-amplify-react-native';
+
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
@@ -12,6 +18,8 @@ export default class App extends React.Component {
     );
   }
 }
+
+export default withAuthenticator(App);
 
 const styles = StyleSheet.create({
   container: {
---
+++ src/aws-exports.js
@@ -0,0 +1,8 @@
+const awsConfig = {
+  identityPoolId: '<Cognito Identity Pool ID>',
+  region: '<Region>',
+  userPoolId: '<Cognito User Pool ID>',
+  userPoolWebClientId: '<Cognito App Client ID>'
+}
+
+export default awsConfig;
